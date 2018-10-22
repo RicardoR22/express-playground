8b57f02c18134b9246ca7ac45d21a9db8d2bda3c
Adam Braus <ajbraus@gmail.com>
2018-10-22 14:59:08 -0700

    fixing

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+// INITIALIZATION
 const express = require('express')
 const exphbs  = require('express-handlebars');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine('.hbs', exphbs({extname: '.hbs'}));
+// MIDDLEWARE
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 app.use(express.static('public'))
 
+
+// ROUTES
 // ROOT ROUTE
 app.get('/', (req, res) => {
-  res.render('home', { stuff: "hey this is some stuff"});
+  res.render('home', { articles: [] });
 })
 
+app.post('/articles', (req, res) => {
+
+})
+
+// STARTING THE SERVER
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
