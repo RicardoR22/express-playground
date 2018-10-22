8920cd5dab1809d877448f0ac6ec54ba184e3ea1
Adam Braus <ajbraus@gmail.com>
2018-10-22 14:30:31 -0700

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs  = require('express-handlebars');
+
+const app = express()
+const port = 3000
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(express.static('public'))
+
+// ROOT ROUTE
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
